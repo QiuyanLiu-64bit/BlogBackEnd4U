93e32c452b166e70bd802d8820c499230ee31f7f
Qiuyan <1837005757@qq.com>
2023-08-20 14:08:35 +0800

    init

---
+++ BlogBackEnd/src/main/java/com/cqucs/blogbackend/BlogBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.cqucs.blogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBackEndApplication.class, args);
+    }
+
+}
