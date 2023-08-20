a51fdc374995d81d5aee8ae7b9a32c6a3374f392
Qiuyan <1837005757@qq.com>
2023-08-20 19:14:48 +0800

    修正提交1

---
+++ BlogBackEnd/src/main/java/com/cqucs/blogbackend/entity/Article.java
@@ -0,0 +1,52 @@
+package com.cqucs.blogbackend.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@ApiModel(value = "文章信息", description="文章信息")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+    @ApiModelProperty(value = "文章编号")
+    private Integer a_id;
+
+    @ApiModelProperty(value = "用户编号")
+    private Integer u_id;
+
+    @ApiModelProperty(value = "分类编号")
+    private Integer cg_id;
+
+    @ApiModelProperty(value = "文章摘要")
+    private String a_tabloid;
+
+    @ApiModelProperty(value = "文章内容")
+    private String a_content;
+
+    @ApiModelProperty(value = "文章标签")
+    private String a_tags;
+
+    @ApiModelProperty(value = "文章标题")
+    private String a_title;
+
+    @ApiModelProperty(value = "文章浏览数")
+    private Integer a_views;
+
+    @ApiModelProperty(value = "文章创建时间")
+    private Date a_create_time;
+
+    @ApiModelProperty(value = "文章发布时间")
+    private Date a_deliver_time;
+
+    @ApiModelProperty(value = "文章更新时间")
+    private Date a_update_time;
+
+    @ApiModelProperty(value = "文章封面")
+    private String a_cover_url;
+}
---
+++ BlogBackEnd/src/main/java/com/cqucs/blogbackend/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.cqucs.blogbackend.entity;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @ApiModelProperty(value = "评论编号")
+    private Integer c_id;
+
+    @ApiModelProperty(value = "用户编号")
+    private Integer u_id;
+
+    @ApiModelProperty(value = "文章编号")
+    private Integer a_id;
+
+    @ApiModelProperty(value = "评论时间")
+    private Date c_time;
+
+    @ApiModelProperty(value = "评论内容")
+    private String c_content;
+}
