91ae25ffd5597580fb5a33f7e6c9fb7332d4295c
Qiuyan <1837005757@qq.com>
2023-08-22 01:10:54 +0800

    验证码超时检测功能

---
+++ BlogBackEnd/src/main/java/com/cqucs/blogbackend/entity/DTO/DynamicsDTO.java
@@ -0,0 +1,32 @@
+package com.cqucs.blogbackend.entity.DTO;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DynamicsDTO {
+    @ApiModelProperty(value = "动态编号")
+    private Integer d_id;
+
+    @ApiModelProperty(value = "用户编号")
+    private Integer u_id;
+
+    @ApiModelProperty(value = "动态内容")
+    private String d_content;
+
+    @ApiModelProperty(value = "动态图片1")
+    private String d_image1;
+
+    @ApiModelProperty(value = "动态图片2")
+    private String d_image2;
+
+    @ApiModelProperty(value = "动态图片3")
+    private String d_image3;
+
+    @ApiModelProperty(value = "动态图片4")
+    private String d_image4;
+}
