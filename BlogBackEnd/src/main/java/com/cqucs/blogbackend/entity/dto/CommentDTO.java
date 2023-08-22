633dfd308274ab6176e28a5b9f0306b5e92822d0
Qiuyan <1837005757@qq.com>
2023-08-22 21:01:32 +0800

    comment相关修改

---
+++ BlogBackEnd/src/main/java/com/cqucs/blogbackend/entity/dto/CommentDTO.java
@@ -0,0 +1,25 @@
+package com.cqucs.blogbackend.entity.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDTO {
+    @ApiModelProperty(value = "用户编号")
+    private Integer u_id;
+
+    @ApiModelProperty(value = "文章编号")
+    private Integer a_id;
+
+    @ApiModelProperty(value = "评论时间")
+    private Date c_time;
+
+    @ApiModelProperty(value = "评论内容")
+    private String c_content;
+}
